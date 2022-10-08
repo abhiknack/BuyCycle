de67179193956a7ee49c63badcf538d5a89ac082
Abhiknack <abhibhai1507@gmail.com>
2022-10-09 01:57:34 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 
 import Header from './components/Layout/Header';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
 
 
 function App() {
