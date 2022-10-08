0c9c1e312e8728c08761f455a9f4c48645525cf5
Abhiknack <abhibhai1507@gmail.com>
2022-10-09 01:42:23 +0530

    Added Files

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,24 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+
+      <div className={classes.back}>
+        <header className={classes.header}>
+          <h1 className={classes.h1}>BuyCycles</h1>
+        </header>
+        <div className={classes.block}>
+          <h2 className={classes.h2}>Currently Under Development</h2>
+        </div>
+        </div>
+
+    </Fragment>
+  );
+};
+
+export default Header;
